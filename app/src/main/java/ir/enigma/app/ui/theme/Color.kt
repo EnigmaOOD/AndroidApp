fd65f28532141ac1620159ae7cd21a97df89509f
Madanian <nouriehmadanian@gmail.com>
2023-04-02 16:13:53 +0330

    set fonts, colors and icons

--- app/src/main/java/ir/enigma/app/ui/theme/Color.kt
+++ app/src/main/java/ir/enigma/app/ui/theme/Color.kt
@@ -2,7 +2,14 @@ package ir.enigma.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val green = Color(0xFF1D7873)
+val red = Color(0xFFFF373B)
+val blue = Color(0xFF284BA5)
+val dark_gray = Color(0xFF1F272E)
+val light_gray = Color(0xFFFAFAFA)
+
+val category_color_1 = Color(0xFFFFB4B4)
+val category_color_2 = Color(0xFFF7C8F5)
+val category_color_3 = Color(0xFFB9F3E4)
+val category_color_4 = Color(0xFFFFE289)
+val category_color_5 = Color(0xFFB4FFB7)
--- app/src/main/java/ir/enigma/app/ui/theme/Theme.kt
+++ app/src/main/java/ir/enigma/app/ui/theme/Theme.kt
@@ -5,35 +5,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    primary = blue,
+    primaryVariant = blue,
+    secondary = green,
+    error = red,
     background = Color.White,
-    surface = Color.White,
+    surface = light_gray,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onSecondary = Color.White,
+    onError = Color.White,
+    onBackground = dark_gray,
+    onSurface = dark_gray,
 )
 
 @Composable
 fun EnigmaAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = LightColorPalette
+
 
     MaterialTheme(
         colors = colors,
