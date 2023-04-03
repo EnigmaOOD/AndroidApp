7c70d0a07483072aba03aa29798b0a582ddaf180
Madanian <nouriehmadanian@gmail.com>
2023-04-04 01:04:31 +0330

    arrow icon with price, user item components

---
+++ app/src/main/java/ir/enigma/app/component/IconTextTF.kt
@@ -0,0 +1,71 @@
+package ir.enigma.app.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import ir.enigma.app.R
+import ir.enigma.app.ui.theme.EnigmaAppTheme
+import ir.enigma.app.ui.theme.IconMedium
+import ir.enigma.app.ui.theme.IconVerySmall
+
+@Composable
+fun IconText(
+    modifier: Modifier = Modifier,
+    trueVar: Boolean,
+    text: String,
+    contentDescription: String? = null
+) {
+    if (trueVar) {
+        Row(modifier = modifier) {
+            Icon(
+                modifier = Modifier
+                    .size(IconVerySmall)
+                    .align(alignment = Alignment.CenterVertically),
+                painter = painterResource(id = R.drawable.ic_arrow_circle_down),
+                contentDescription = contentDescription,
+                tint = MaterialTheme.colors.secondary
+            )
+            TextSubtitle2(
+                modifier = Modifier.padding(start = 6.dp),
+                text = "$text تومان",
+                color = MaterialTheme.colors.secondary
+            )
+        }
+    } else {
+        Row(modifier = modifier) {
+            Icon(
+                modifier = Modifier
+                    .size(IconVerySmall)
+                    .align(alignment = Alignment.CenterVertically),
+                painter = painterResource(id = R.drawable.ic_arrow_circle_up),
+                contentDescription = contentDescription,
+                tint = MaterialTheme.colors.error
+            )
+            TextSubtitle2(
+                modifier = Modifier.padding(start = 6.dp),
+                text = "$text تومان",
+                color = MaterialTheme.colors.error
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun prev() {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        EnigmaAppTheme {
+            IconText(trueVar = true, text = "16000")
+        }
+    }
+}
---
+++ app/src/main/java/ir/enigma/app/component/UserItem.kt
@@ -0,0 +1,91 @@
+package ir.enigma.app.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import ir.enigma.app.R
+import ir.enigma.app.ui.theme.EnigmaAppTheme
+import ir.enigma.app.ui.theme.IconDefaultPadding
+import ir.enigma.app.ui.theme.IconMedium
+
+@Composable
+fun UsrItem(
+    modifier: Modifier = Modifier,
+
+    CModifier: Modifier = Modifier,
+    Cicon: Boolean,
+    Cresource: Int,
+    Csize: Dp = IconMedium,
+    CcontentPadding: Dp = IconDefaultPadding,
+    Ctint: Color = MaterialTheme.colors.onBackground,
+    Celevation: Dp = 0.dp,
+    CbackgroundColor: Color = MaterialTheme.colors.background,
+    CcontentDescription: String? = null,
+
+    Tmodifier: Modifier = Modifier,
+    Ttext: String,
+    Tcolor: Color = MaterialTheme.colors.onBackground,
+
+    Imodifier: Modifier = Modifier,
+    ItrueVar: Boolean,
+    Itext: String,
+    IcontentDescription: String? = null
+) {
+    Row(modifier = modifier) {
+        CardWithImageOrIcon(
+            modifier = CModifier.align(alignment = Alignment.CenterVertically),
+            icon = Cicon,
+            resource = Cresource,
+            size = Csize,
+            contentPadding = CcontentPadding,
+            tint = Ctint,
+            elevation = Celevation,
+            backgroundColor = CbackgroundColor,
+            contentDescription = CcontentDescription
+        )
+
+        TextH6(
+            text = Ttext,
+            modifier = Tmodifier
+                .padding(start = 12.dp)
+                .weight(1f)
+                .align(alignment = Alignment.CenterVertically),
+            color = Tcolor
+        )
+
+        IconText(
+            modifier = Imodifier.align(alignment = Alignment.CenterVertically),
+            trueVar = ItrueVar,
+            text = Itext,
+            contentDescription = IcontentDescription
+        )
+    }
+}
+
+@Preview()
+@Composable
+fun Previ() {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        EnigmaAppTheme {
+            UsrItem(
+                Cicon = false,
+                Cresource = R.drawable.ic_fill_birthday_cake,
+                Csize = 50.dp,
+                CcontentPadding = 12.dp,
+                Ttext = "علی علوی",
+                ItrueVar = false,
+                Itext = "16000"
+            )
+        }
+    }
+}
